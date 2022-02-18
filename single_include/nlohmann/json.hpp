4c897747ec1a94b962dd1fc7d9ac6fe051e26483
Qianqian Fang <fangqq@gmail.com>
2022-02-18 15:22:19 -0500

    add tests for optimized ndarray, improve coverage, fix clang/gcc warnings

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -10071,6 +10071,9 @@ class binary_reader
                             uint64_t len{};
                             return get_number(input_format, len) && get_string(input_format, len, result);
                         }
+                        default:
+                        {
+                        }
                     }
                 }
                 auto last_token = get_token_string();
@@ -10087,9 +10090,12 @@ class binary_reader
         std::pair<std::size_t, char_int_type> size_and_type;
         size_t dimlen = 0;
 
-        bool is_optimized = get_ubjson_size_type(size_and_type);
+        if (JSON_HEDLEY_UNLIKELY(!get_ubjson_size_type(size_and_type)))
+        {
+            return false;
+        }
 
-        if (is_optimized && size_and_type.first != string_t::npos)
+        if (size_and_type.first != string_t::npos)
         {
             if (size_and_type.second != 0)
             {
@@ -10121,7 +10127,7 @@ class binary_reader
         {
             while (current != ']')
             {
-                if (JSON_HEDLEY_UNLIKELY(!get_ubjson_size_value(dimlen)))
+                if (JSON_HEDLEY_UNLIKELY(!get_ubjson_size_value(dimlen, current)))
                 {
                     return false;
                 }
@@ -10233,7 +10239,7 @@ class binary_reader
                             {
                                 return false;
                             }
-                            result = static_cast<std::size_t>(number);
+                            result = number;
                             return true;
                         }
                         case '[':
@@ -10250,11 +10256,14 @@ class binary_reader
                             }
                             return true;
                         }
+                        default:
+                        {
+                        }
                     }
                 }
-                auto last_token = get_token_string();
-                return sax->parse_error(chars_read, last_token, parse_error::create(113, chars_read, exception_message(input_format, "expected length type specification (U, i, I, l, L) after '#'; last byte: 0x" + last_token, "size"), BasicJsonType()));
             }
+            auto last_token = get_token_string();
+            return sax->parse_error(chars_read, last_token, parse_error::create(113, chars_read, exception_message(input_format, "expected length type specification (U, i, I, l, L) after '#'; last byte: 0x" + last_token, "size"), BasicJsonType()));
         }
     }
 
@@ -10407,27 +10416,27 @@ class binary_reader
                     {
                         case 'u':
                         {
-                            uint16_t number;
+                            uint16_t number{};
                             return get_number(input_format, number) && sax->number_unsigned(number);
                         }
                         case 'm':
                         {
-                            uint32_t number;
+                            uint32_t number{};
                             return get_number(input_format, number) && sax->number_unsigned(number);
                         }
                         case 'M':
                         {
-                            uint64_t number;
+                            uint64_t number{};
                             return get_number(input_format, number) && sax->number_unsigned(number);
                         }
                         case 'h':
                         {
-                            const int byte2 = get();
+                            unsigned int byte2 = get();
                             if (JSON_HEDLEY_UNLIKELY(!unexpect_eof(input_format, "half")))
                             {
                                 return false;
                             }
-                            const int byte1 = get();
+                            unsigned int byte1 = get();
                             if (JSON_HEDLEY_UNLIKELY(!unexpect_eof(input_format, "half")))
                             {
                                 return false;
@@ -10441,11 +10450,11 @@ class binary_reader
                             // without such support. An example of a small decoder for
                             // half-precision floating-point numbers in the C language
                             // is shown in Fig. 3.
-                            const int half = (byte1 << 8) + byte2;
+                            unsigned int half = (byte1 << 8) + byte2;
                             const double val = [&half]
                             {
-                                const int exp = (half >> 10) & 0x1F;
-                                const int mant = half & 0x3FF;
+                                unsigned int exp = (half >> 10) & 0x1F;
+                                unsigned int mant = half & 0x3FF;
                                 JSON_ASSERT(0 <= exp&& exp <= 32);
                                 JSON_ASSERT(0 <= mant&& mant <= 1024);
                                 switch (exp)
@@ -10463,6 +10472,9 @@ class binary_reader
                             return sax->number_float((half & 0x8000) != 0
                                                      ? static_cast<number_float_t>(-val)
                                                      : static_cast<number_float_t>(val), "");
+                        }
+                        default:
+                        {
                         }
                     }
                 }
@@ -10899,7 +10911,7 @@ class binary_reader
     const bool is_little_endian = little_endianness();
 
     /// input format
-    input_format_t input_format;
+    input_format_t input_format = input_format_t::json;
 
     /// the SAX parser
     json_sax_t* sax = nullptr;
